680d79237959fc61105738026722bce54aa6714a
Renat Khizbullin <r.khizbullin@intelli-vision.com>
2019-03-03 12:48:40 +0300

    Fix evaluation

--- evaluation.py
+++ evaluation.py
@@ -1,21 +1,26 @@
 import torch
+import argparse
 
 from utils import load_config
 
+parser = argparse.ArgumentParser(description='Evaluate trained detector')
+parser.add_argument('--config', required=True, type=str, help='Path to model config')
+parser.add_argument('--checkpoint', required=True, type=str, help='Path to model')
+args = parser.parse_args()
+
+
 if __name__ == '__main__':
-    config = load_config("configs/config.py")
+    config = load_config(args.config)
 
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     if torch.cuda.is_available():
         torch.backends.cudnn.benchmark = True
 
-    checkpoint_path = "mobilenet_ssd/19-02-14_00-07/saved/weights_iter138000.pth"
-
-    model = torch.load(checkpoint_path, map_location="cpu")
+    model = torch.load(args.checkpoint, map_location="cpu")
     model.to(DEVICE)
 
     with torch.no_grad():
         model.eval()
         params = {"tensorboard": False}
-        config.validator(model, params)
+        config.validator(config.val_loader, model, params)
